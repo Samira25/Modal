7c4dd199fee5e8bd461e863d7f3cabbededd1bc4
Samira Mohana <26239566+Samira25@users.noreply.github.com>
2017-11-24 18:37:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function video1(){
+	var video1 = $(".vid1").attr("src");
+    $(".vid1").attr("src","");
+    $(".vid1").attr("src",video1);
+}
+function video2(){
+	var video2 = $(".vid2").attr("src");
+    $(".vid2").attr("src","");
+    $(".vid2").attr("src",video2);
+}
